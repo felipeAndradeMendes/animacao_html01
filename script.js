2c5b6e21f062becdd4edaa8c6aafd35cf63d4f89
Felipe Madruga <99842190+felipeMadruga@users.noreply.github.com>
2022-03-15 19:04:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,4 @@ function moverUp(){                         // Função que contém o setInterva
 }
 
 
-/* README
-Por falta de tempo, o desenvolvimento desse pequeno projeto de treino, não avançou a ponto de aperfeiçoar vários detalhes.
-Por exemplo, o valor numerico inserido na caixa de numeros pode ser alterado a qualquer momento, porém só terá efeito quando o elemento
-atingir a margem extrema do elemento pai. Essa parte não foi trabalhada. */
+
